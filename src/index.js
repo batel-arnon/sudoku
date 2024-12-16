7067dedaa6aabb69cc997d63a69c0da84e4a274d
batel <batelarnon06@gmail.com>
2024-12-16 19:47:24 +0200

    addind extra

--- src/index.js
+++ src/index.js
@@ -163,13 +163,30 @@ class Game extends React.Component {
     }
     return false;
   }
+  isExtra(index, value){
+    const cubes = [[0,1,2,9,10,11,18,19,20],[3,4,5,12,13,14,21,22,23],[6,7,8,15,16,17,24,25,26],
+    [0+27,1+27,2+27,9+27,10+27,11+27,18+27,19+27,20+27],[3+27,4+27,5+27,12+27,13+27,14+27,21+27,22+27,23+27],[6+27,7+27,8+27,15+27,16+27,17+27,24+27,25+27,26+27],
+    [0+54,1+54,2+54,9+54,10+54,11+54,18+54,19+54,20+54],[3+54,4+54,5+54,12+54,13+54,14+54,21+54,22+54,23+54],[6+54,7+54,8+54,15+54,16+54,17+54,24+54,25+54,26+54]]
+    let index_place_in_the_cobe;
+    for (let i = 0; i < cubes.length; i++) {
+      if (cubes[i].includes(index)){
+        index_place_in_the_cube = cubes[i].indexOf(index);
+      }
+    }
+    for (let i = 0; i < cubes.length; i++) {
+      if (this.state.squares[cubes[i][index_place_in_the_cobe]]===value){
+        return true;
+      }
+    }
+    return false;
+  }
   insertValueToSquares(index, value){
     const squares = this.state.squares;
     squares[index] = value;
     this.setState({squares: squares})
   }
   isValid(index, value){
-    return !(this.isAnah(index, value) || this.isCube(index, value) || this.isOfek(index, value));
+    return !(this.isAnah(index, value) || this.isCube(index, value) || this.isOfek(index, value)||this.isExtra(index, value));
   }
   isFull(squares){
     for (let i = 0; i < this.state.squares.length; i++) {
